dcadd38591a732542f31bababafcd1d95fa05c48
meilan <meilanulfia@gmail.com>
2025-05-13 19:05:56 +0700

    dengan akses public constructorOverLoading

--- constructorOverLoading.cpp
+++ constructorOverLoading.cpp
@@ -7,4 +7,10 @@ class mahasiswa{
     int nim;
     string nama;
 
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
 };
